80b8f15ac0ffa619762129258654586e4aaa041d
djdeo <deoseoer@gmail.com>
2021-01-19 09:52:04 +0800

    feat: add pagination

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { QueryClient, QueryClientProvider } from 'react-query'
+// import { ReactQueryDevtools } from "react-query-devtools";
 
+const queryClient = new QueryClient()
 function App() {
   const [page, setPage] = useState('planets');
 
   return (
-    <div className="App">
-     <h1>Star Wars Info</h1>
-     <Navbar setPage={setPage} />
-     <div className="content">
-       { page === 'planets' ? <Planets /> : <People /> }
-     </div>
-    </div>
+    <>
+      <div className="App">
+       <h1>Star Wars Info</h1>
+       <Navbar setPage={setPage} />
+       <QueryClientProvider client={queryClient}>
+       <div className="content">
+         { page === 'planets' ? <Planets /> : <People /> }
+       </div>
+       </QueryClientProvider>
+      </div>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </>
   );
 }
 
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+import {useQuery, useInfiniteQuery } from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async (page) => {
+    const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+    return res.json();
+}
+
+
 const Planets = () => {
+    const [page, setPage] = useState(1);
+    const {data, isLoading, error, isPreviousData } = useQuery(['planets', page], () => fetchPlanets(page), {
+        keepPreviousData:true,
+    });
+        // https://react-query.tanstack.com/guides/paginated-queries#better-paginated-queries-with-keeppreviousdata
     return (
         <div>
             <h2>Planets</h2>
+
+            {isLoading && (<div>Loading data...</div>)}
+            {error && (<div>Error fetching data</div>)}
+
+            { (error === null) && (
+                <>
+                    <button onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}  >Prev</button>
+                    <span> {page}</span>
+                    <button onClick={() => {
+                        if(!isPreviousData && data.next) {
+                            setPage(old => old+1)
+                        }
+                    }} disabled={isPreviousData || !data.next}  >Next</button>
+                    <div>
+                    {data?.results?.map(planet => <Planet key={planet.name} planet={planet} />)}
+                    </div>
+                </>
+            )}
         </div>
     );
 }
